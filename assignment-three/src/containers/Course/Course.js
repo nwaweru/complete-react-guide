e0844f6b88badccad0e63695792d7dd4b08950f7
Ndirangu Waweru <ndiranguwaweru@gmail.com>
2019-09-23 18:44:10 +0300

    completed assignment 3

---
+++ assignment-three/src/containers/Course/Course.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+
+class Course extends Component {
+    state = {
+        title: ''
+    };
+
+    componentDidMount() {
+        this.setTitle();
+    }
+
+    componentDidUpdate() {
+        this.setTitle();
+    }
+
+    setTitle() {
+        const query = new URLSearchParams(this.props.location.search);
+
+        for (let param of query.entries()) {
+            if (this.state.title !== param[1]) {
+                this.setState({ title: param[1] });
+            }
+        }
+    }
+
+    render () {
+        return (
+            <div>
+                <h1>{this.state.title}</h1>
+                <p>You selected the Course with ID: {this.props.match.params.id}</p>
+            </div>
+        );
+    }
+}
+
+export default Course;
